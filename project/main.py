c756c7697dddf5f97a36d3e8caa6d31483bdbd04
lucky-KRMU <2501730142@krmu.edu.in>
2026-06-22 10:07:47 +0000

    add sigmoid function in Network

--- project/main.py
+++ project/main.py
@@ -16,9 +16,15 @@ class Network:
         self.net_size: list = net_size
         self.layer_num: int = len(net_size)
         
-        self.biases = [ np.random.randn(y, 1) for y in (net_size[1:]) ] # Defining the biases of the entire network
+        self.biases = [ np.random.randn(y, 1) for y in (net_size[1:]) ] # Defining the biases of the entire network\
         self.weights = [ np.random.randn(y,x) for y,x in zip(net_size[ 1: ], net_size[ :-1 ]) ] # Definging the weights for each layer of the entire network
-        print(self.biases,'\n',self.weights)
         
+    # The sigmoid/activatoin function
+    def sigmoid(self, z: float):
+        return 1/( 1 + np.exp(-z) )
+    
+    
+        
+    
 N = Network([784, 50, 10])
         
