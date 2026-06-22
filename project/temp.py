9180799e1ad9b92f356d46e52b3a046d9e10b5e4
lucky-KRMU <2501730142@krmu.edu.in>
2026-06-22 09:10:25 +0000

    add training data

---
+++ project/temp.py
@@ -0,0 +1,10 @@
+'''
+This is a python file which is made to run small experimental code before getting implemented in the origin code for project
+'''
+
+
+import numpy as np
+
+a = np.random.randn(2, 2)
+print(type(a))
+print(a)
