082b9088d9dc8134a93c4fae31b2897d252c86a5
lucky-KRMU <2501730142@krmu.edu.in>
2026-06-22 15:21:59 +0530

    fix biases initiation bug

--- project/main.py
+++ project/main.py
@@ -16,6 +16,9 @@ class Network:
         self.net_size: list = net_size
         self.layer_num: int = len(net_size)
         
-        self.biases = [ np.random.randn(y, 1) for y in range(net_size[1:]) ] # Defining the biases of the entire network
+        self.biases = [ np.random.randn(y, 1) for y in (net_size[1:]) ] # Defining the biases of the entire network
         self.weights = [ np.random.randn(y,x) for y,x in zip(net_size[ 1: ], net_size[ :-1 ]) ] # Definging the weights for each layer of the entire network
+        print(self.biases,'\n',self.weights)
+        
+N = Network([784, 50, 10])
         
